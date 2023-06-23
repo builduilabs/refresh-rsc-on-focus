4384364156bbc377e8878b3276e4167d30c6799c
Sam Selikoff <sam.selikoff@gmail.com>
2023-06-23 13:57:41 -0400

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="antialiased font-sans bg-white">{children}</body>
+    <html lang="en" className="h-full">
+      <body className="text-gray-900 antialiased font-sans bg-white h-full">
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,35 +9,31 @@ export default function Home() {
   let date = new Date();
 
   return (
-    <div className="m-3 sm:m-6">
-      <h1 className="font-medium text-gray-900 text-lg tracking-tight">
-        Refresh RSC on focus
+    <div className="max-w-prose mx-auto w-full min-h-full flex flex-col justify-center">
+      <h1 className="font-bold text-4xl tracking-tight">
+        Refresh RSC On Focus
       </h1>
-      <div className="text-gray-900 max-w-prose mt-3">
+      <div className="mt-3 text-lg">
         This page shows the datetime when this server component was rendered.
         Try refocusing this browser window to trigger a server side rerender.
       </div>
 
-      <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:space-x-12 sm:items-center mt-6 sm:mt-8">
+      <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:space-x-12 sm:items-center mt-12">
         <div>
           <div className="text-gray-500 text-sm">Date</div>
-          <div className="text-3xl font-bold tracking-tighter text-gray-900">
+          <div className="text-3xl font-bold tracking-tighter">
             {formatInTimeZone(date, tz, "MMMM do, yyyy")}
           </div>
         </div>
-
         <div>
           <div className="text-gray-500 text-sm">Time</div>
-          <div className="text-3xl font-bold tracking-tighter text-gray-900">
+          <div className="text-3xl font-bold tracking-tighter">
             {formatInTimeZone(date, tz, "h:mm:ss a")}
           </div>
         </div>
-
         <div>
           <div className="text-gray-500 text-sm">Timezone</div>
-          <div className="text-3xl font-bold tracking-tighter text-gray-900">
-            {formatInTimeZone(date, tz, "zzz")}
-          </div>
+          <div className="text-3xl font-bold tracking-tighter">{tz}</div>
         </div>
       </div>
       <RefreshOnFocus />
